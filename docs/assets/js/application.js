25de534c9bcc9b00ce5292d47cdb5ac654c0c11e
uvu <setkyar16@gmail.com>
2014-03-29 11:04:10 +0630

    Version 1.1 For Laravel4.1-Chrome-Extension

---
+++ docs/assets/js/application.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+
+    // prettyprint
+    $('pre').addClass('prettyprint');
+
+	// uniform
+	$('select, input:checkbox, input:radio, input:file').uniform();
+
+});
